6e02183fd7c284830e7cb2b8818ef46c0fa943e2
UnknownChick <ferreira.alexandre.2000@outlook.com>
2021-11-08 21:19:20 +0100

    Mise en place du formulaire d inscription

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +16,40 @@ class RegisterType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
+            ->add('firstname', TextType::class, [
+                'label' => 'Votre prénom',
+                'attr' => [
+                    'placeholder' => 'Saisir votre prénom'
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Votre nom',
+                'attr' => [
+                    'placeholder' => 'Saisir votre nom'
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Votre email',
+                'attr' => [
+                    'placeholder' => 'Saisir votre email'
+                ]
+            ])
+            ->add('password',  PasswordType::class, [
+                'label' => 'Votre mot de passe',
+                'attr' => [
+                    'placeholder' => 'Saisir votre mot de passe'
+                ]
+            ])
+            ->add('password_confirm', PasswordType::class, [
+                'label' => 'Confirmer votre mot de passe',
+                'mapped' => false,
+                'attr' => [
+                    'placeholder' => 'Saisir à nouveau votre mot de passe'
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'S\'inscrire'
+            ])
         ;
     }
 
